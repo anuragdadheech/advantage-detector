829d45a055d9c85dd8bd0c989cb998ef9fbee1e9
Anurag Dadheech <anuragdadheech1990@gmail.com>
2015-02-07 21:48:30 +0530

    detected first load of items on search and consecutive each loads

--- plugin/fka.js
+++ plugin/fka.js
@@ -1,16 +1,39 @@
 /*global $:false, chrome */
 (function(){
 	"use strict";
-	chrome.devtools.network.onRequestFinished.addListener(
-		function(request) {
-			if (request.response) {
-				chrome.experimental.devtools.console.addMessage(
-					"URL: " + request.request.url
-				);
-			}
+	console.log("FK Advantage plugin");
+	window.fka_pageCount = 2;
+	window.fka_firstLoad = true;
+	// chrome.devtools.network.onRequestFinished.addListener(
+	// 	function(request) {
+	// 		alert("works");
+	// 		console.log("FK Advantage plugin");
+	// 		if (request.response) {
+	// 			chrome.experimental.devtools.console.addMessage(
+	// 				"Fk advantage | URL: " + request.request.url
+	// 			);
+	// 		}
 			
-		});
+	// 	});
+	$("#products").bind("DOMSubtreeModified", function() {
+		//First time products loading
+		if($("#products").length && $("a.fk-product-thumb").length && window.fka_firstLoad) {
+			window.fka_firstLoad = false;
+			console.log("Flipkart Advantage plugin | First page updated");
+		}
+
+
+		//Taking into account dynamic loading
+		if($("#page-"+window.fka_pageCount).length) {
+			console.log("Flipkart Advantage plugin | Page updated | Added page "+window.fka_pageCount);
+			window.fka_pageCount++;	
+		}
+
+		
+	});
 
 }());
+
+// $("a.fk-product-thumb").length
 
  
